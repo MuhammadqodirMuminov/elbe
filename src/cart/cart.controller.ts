f95d67aee87150b10000c0afdc59f48bbfcc31d0
MuhammadqodirMuminov <muhammadqodirmuminov2@gmail.com>
2024-07-06 00:14:20 +0500

    update carts

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,43 +1,48 @@
 // src/cart/cart.controller.ts
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { CurrentUser } from 'src/auth/current-user.decorator';
+import JwtAuthGuard from 'src/auth/guards/jwt-auth.guard';
+import { UserDocument } from 'src/auth/users/models/user.schema';
 import { CartService } from './cart.service';
-import { CreateCartDto, UpdateCartDto } from './dto/create-cart.dto';
+import { UpdateCartDto } from './dto/create-cart.dto';
 import { CartDocument } from './models/cart.schema';
 
 @ApiTags('cart')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('cart')
 export class CartController {
     constructor(private readonly cartService: CartService) {}
 
-    @Post()
-    async create(@Body() createCartDto: CreateCartDto): Promise<CartDocument> {
-        return this.cartService.create(createCartDto);
-    }
-
-    @Get(':userId')
-    async findByUser(@Param('userId') userId: string): Promise<CartDocument> {
-        return this.cartService.findByUser(userId);
-    }
-
     @Get()
-    async findAll(): Promise<CartDocument[]> {
-        return this.cartService.findAll();
+    async create(@CurrentUser() user: UserDocument): Promise<CartDocument> {
+        return this.cartService.getOrcreate(user);
     }
 
-    @Post(':userId/add-item')
-    async addItem(@Param('userId') userId: string, @Body() updateCartDto: UpdateCartDto): Promise<CartDocument> {
-        return this.cartService.addItem(userId, updateCartDto);
+    @Post('upsert-item')
+    async updateActiveCart(
+        @Body() itemUpdateDto: UpdateCartDto,
+        @CurrentUser()
+        user: UserDocument,
+    ) {
+        return this.cartService.updateActiveCart(user, itemUpdateDto);
     }
 
-    @Delete(':userId/remove-item/:productId')
-    async removeItem(@Param('userId') userId: string, @Param('productId') productId: string): Promise<CartDocument> {
-        return this.cartService.removeItem(userId, productId);
+    @Post('empty-cart')
+    async emptyActiveCart(
+        @CurrentUser()
+        user: UserDocument,
+    ) {
+        return this.cartService.emptyActiveCart(user);
     }
 
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @Delete('clear/:userId')
-    async clearcart(@Param('userId') userId: string): Promise<void> {
-        return await this.cartService.clearCart(userId);
+    @Delete('cart-items/:id')
+    async removeCart(
+        @Param('id') cartItemId: string,
+        @CurrentUser()
+        user: UserDocument,
+    ) {
+        return this.cartService.removeItem(cartItemId, user);
     }
 }
--- src/cart/models/cart.schema.ts
+++ src/cart/models/cart.schema.ts
@@ -2,23 +2,21 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
 import { UserDocument } from 'src/auth/users/models/user.schema';
 import { AbstractDocument } from 'src/database/abstract.schema';
+import { CartItemsDocument } from './car-item.schema';
 
-@Schema({ versionKey: false, timestamps: false })
+@Schema({ versionKey: false, timestamps: true })
 export class CartDocument extends AbstractDocument {
     @Prop({ type: Types.ObjectId, ref: UserDocument.name, required: true })
     user: Types.ObjectId;
 
     @Prop({
-        type: [
-            {
-                productId: { type: Types.ObjectId, required: true },
-                quantity: { type: Number, required: true },
-                _id: false,
-            },
-        ],
-        required: true,
+        type: [{ type: Types.ObjectId, ref: CartItemsDocument.name, required: false }],
+        required: false,
     })
-    items: { productId: Types.ObjectId; quantity: number }[];
+    items?: CartItemsDocument[];
+
+    @Prop({ type: Boolean, required: false, default: false })
+    is_order_created?: boolean;
 }
 
 export const CartSchema = SchemaFactory.createForClass(CartDocument);
